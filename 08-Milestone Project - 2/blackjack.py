36823f3e735657bca247885a1f838403afc6eca8
bvolger <bvolger@gmail.com>
2022-10-14 00:46:07 +0200

    Blackjack update or something. IDK, havent touched this stuff for a while

--- 08-Milestone Project - 2/blackjack.py
+++ 08-Milestone Project - 2/blackjack.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-import random
+import random, sys, os
 
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
@@ -39,16 +39,27 @@ class Player():
         self.balance = balance
         self.cards = []
         if self.name != 'Dealer':
-            print(f"Welcome {self.name}. Your starting balance is {self.balance}")
+            print(f"\nWelcome {self.name}. Your starting balance is {self.balance}")
 
-    def bet(self, amount):
-        if self.balance >= amount:
-            self.balance -= amount
+    def bet(self, **kwargs):
+        if 'wager' in kwargs:
+            self.amount = kwargs['wager']
+
+            if self.balance >= self.amount:
+            #self.balance -= amount
+                return self.amount
+            else:
+                print("Not enough money in account!")
+                return None
         else:
-            print("Not enough money in account!")
+            return self.amount
 
-    def winnings(self, amount):
-        self.balance += amount
+
+    def winnings(self, amount, won):
+        if won == True:
+            self.balance += amount
+        else:
+            self.balance -= amount
 
     def balance(self):
         return self.balance
@@ -57,107 +68,56 @@ class Player():
         for card in self.cards:
             print(card)
 
+    def hand_value(self):
+        value = 0
+        # Keep track of aces
+        aces = 0 
+        for card in self.cards:
+            value += card.value
+            if card.rank == 'Ace':
+                aces += 1
+
+        # Reduce value for aces (if necessary)
+        if value > 21 and aces != 0:
+            value -= aces * 10
+
+        return value
+
     def __str__(self):
-        return f"{self.name} has {self.balance} left in account"
+        return f"\n{self.name} has {self.balance} left in account"
 
-def check_hand(player):
-    aces = 0
-    hand_value = 0
 
-    for card in player.cards:
-        hand_value += card.value
-        if card.rank == 'Ace':
-            aces += 1
+    def __len__(self):
+        return len(self.cards)
 
-    if hand_value == 21:
-        return 21
-    elif hand_value > 21 and aces != 0:
-        hand_value -= 10*aces
-        #return hand_value
-    
-    if hand_value > 21:
-        return 'Bust'
-    else:
-        return hand_value
 
-def reset(*players):
+def MyFn(t):
+    return t[0]
+
+
+def reset(*args):
     deck = Deck()
-    for player in players:
-        player.cards = []
+    
+    for entry in args:
+        if type(entry) == dict:
+            for name, player in entry.items():
+                player.cards = []
+        else:
+            entry.cards = []
+
+    #for player in players:
+        #player.cards = []
 
 
 def turn(player, cards):
-    
+
+    print(f"\n{player.name}, cards dealt:")
     for i in range(cards):
-        player.cards.append(deck.deal_card())
-    
-    print(f"\n{player.name} got:")
-    player.show_hand()
-    hand_worth = check_hand(player)
-    print(f"Worth of hand: {hand_worth}")
-    return hand_worth
+        dealt_card = deck.deal_card()
+        print(f">> {dealt_card} <<")
+        player.cards.append(dealt_card)
 
-def play_rotation(**players):
-
-    # First, set a betting amount
-    while True:
-        try:
-            betting_amount = int(input(f"How much do you want to wager? (1-{player.balance}) "))
-        except:
-            print("Integer please..")
-        else:
-            player.bet(betting_amount)
-            break
-
-    player_done = False
-    # Game begins
-    while player_done == False:
-        # Always start with two cards to the player, and one card to the dealer
-        player_value = turn(player, 2)
-        dealer_value = turn(dealer, 1)
-        
-        # Then, players turn
-        while True:
-            if player_value == 21 or player_value == 'Bust':
-                player_done = True
-                break
-            choice = input((f"\n{player.name}, it's your turn. Do you want to hit or stand? ")).lower()
-            if not choice in ['hit', 'stand']:
-                print("Please enter 'hit' or 'stand'")
-                continue
-
-            if choice == 'hit':
-                player_value = turn(player, 1)
-            else:
-                player_done = False
-                break
-
-    # Dealers turn
-    while True:
-    #while dealer_value < 17 and dealer_value != 'Bust':
-        dealer_value = turn(dealer, 1)
-        if dealer_value == 'Bust':
-            break
-        elif dealer_value >= 17:
-            break
-        else:
-            continue
-    #break
-
-    if dealer_value == 'Bust':
-        print(f"Dealer went bust. {player.name} wins!")
-        player.winnings(betting_amount*2)
-    elif player_value == 'Bust':
-        print(f"{player.name} went bust")
-    elif player_value > dealer_value:
-        print(f"{player.name} wins!")
-        player.winnings(betting_amount*2)
-    elif player_value < dealer_value:
-        print(f"Dealer wins!")
-    elif player_value == dealer_value:
-        print(f"It's a draw..")
-        player.winnings(betting_amount)
-
+    return player.hand_value()
 
 # Start playing
 if __name__ == "__main__":
@@ -165,34 +125,157 @@ if __name__ == "__main__":
     # init the deck
     deck = Deck()
 
-    """
-    # Ask for player name, and the amount of betting money
-    name = input("What's your name? ")
     while True:
         try:
-            money = int(input("How much money did you bring? "))
-        except:
-            print("Please provide an integer..")
-        else:
-            break
-
-    player = Player(name, money)
-    """
-    player = Player('Bram', 100)
-    dealer = Player()
-
-    while True:
-        play_rotation()
-        print("\n")
-        print(player)
-
-        choice = input(f"Do you wish to play on? [Y/N] ").lower()
-        if not choice in ['y', 'n']:
-            print("Y/N please..")
-            
-        if choice == 'y':
-            reset(player, dealer)
+            num_of_players = int(input("How many players are playing? "))
+            #test = int(num_of_players)
+        except TypeError:
+            print("Integer please..")
             continue
         else:
             break
+
+    players = {}
+    for i in range(1, num_of_players + 1):
+        player_name = input("\nWhat is the name of player %d? " % i)
+        #while True:
+            #try:
+                #money = int(input(f"And how much money did {player_name} bring? "))
+            #except TypeError:
+                #print("Integer please...")
+            #else:
+                #break
+
+        players[player_name] = Player(player_name, 100)
+        #print(players[player_name])
+        
+    # Dealer guy
+    dealer = Player()
     
+    # Game controlling loop
+    while True:
+
+        # Round starts with all players setting their bets
+        for name,player in sorted(players.items(), key=MyFn):
+            print(player)
+            # First, set a betting amount
+            betting_amount = None
+            while betting_amount == None:
+                try:
+                    betting_amount = int(input(f"How much do you want to wager? (1-{player.balance}) "))
+                    betting_amount = player.bet(wager=betting_amount)
+                except ValueError:
+                    print("Integer please..")
+                except KeyboardInterrupt:
+                    print("\n\nQuitting")
+                    sys.exit(1)
+
+        os.system('clear')
+
+        # Then, two cards to each player, and a card to the dealer
+        players_done = False
+        for name,player in sorted(players.items(), key=MyFn):
+            turn(player, 2)
+        turn(dealer, 1)
+
+        # Then, hitting begins
+        all_players_done = False
+        while all_players_done == False:
+            
+            for name,player in players.items():
+                player_done = False
+                
+                print(f"\n++++++++++++++++++ {player.name}, it's your turn ++++++++++++++++++")
+                while player_done == False:
+                    print(f"\n{player.name}, your hand:")
+                    player.show_hand()
+
+                    hand_value = player.hand_value()
+                    
+                    if hand_value >= 21:
+                        if hand_value == 21 and len(player) == 2:
+                            print(">>>>> BLACKJACK! <<<<<")
+                        elif hand_value == 21:
+                            print(">>>>> 21! <<<<<")
+                        else:
+                            print(">>>>> BUST! <<<<<")
+                        player_done = True
+                        break
+
+
+                    choice = input((f"\nDo you want to hit or stand? ")).lower()
+                    if not choice in ['hit', 'stand']:
+                        print("Please enter 'hit' or 'stand'")
+                        continue
+                    if choice == 'hit':
+                        turn(player, 1)
+                        #hand_value = turn(player, 1)
+                    else:
+                        player_done = True
+                        break
+
+            all_players_done = True
+
+        # Then, dealers turn
+        # Dealers turn
+        while dealer.hand_value() < 17:
+            turn(dealer, 1)
+
+        # Show final hands
+        print('\n=========================================================================\n')
+        print("\nFinal hands:")
+        for name, player in players.items():
+            print(f"\n{name} wagered {player.bet()} and had hand value {player.hand_value()}")
+            player.show_hand()
+
+        print(f"\nDealer: {dealer.hand_value()}")
+        dealer.show_hand()
+
+        print("\n")
+        for name, player in players.items():
+            # Blackjack always wins, unless dealer has one too
+            if player.hand_value() == 21 and len(player) == 2:
+                if dealer.hand_value() == 21 and len(dealer) == 2:
+                    # It's a draw
+                    print(f"Both {player.name} and Dealer hit blackjack. It's a tie")
+                else:
+                    print(f"{player.name} had blackjack and wins from Dealer!")
+                    player.winnings(player.bet(), True)
+            # Player has 21, but dealer has blackjack
+            elif player.hand_value() == 21 and len(player) > 2 and dealer.hand_value() == 21 and len(dealer) == 2:
+                print(f"Dealer had blackjack, {player.name} did not and loses!")
+                player.winnings(player.bet(), False)
+            # Player went bust, always lose
+            elif player.hand_value() > 21:
+                print(f"{player.name} went bust!")
+                player.winnings(player.bet(), False)
+            elif player.hand_value() == dealer.hand_value():
+                print(f"{player.name} and dealer tie!")
+                # No money won or lost
+            elif player.hand_value() < dealer.hand_value():
+                print(f"{player.name} loses from dealer!")
+                player.winnings(player.bet(), False)
+            else:
+                print(f"{player.name} wins from dealer!")
+                player.winnings(player.bet(), True)
+
+        broke = False
+        for player in players.values():
+            print(f"{player}")
+            if player.balance <= 0:
+                print(f"{player.name} went broke. Game ends!")
+                broke = True
+            
+        if broke == True:
+            sys.exit(1)
+        while True:
+            choice = input("Do you wish to play on? [Y/N] ").lower()
+            if not choice in ['y', 'n']:
+                print("Y/N please..")
+            if choice == 'y':
+                reset(players, dealer)
+                #reset(dealer)
+                os.system('clear')
+                break
+            else:
+                sys.exit(1)
